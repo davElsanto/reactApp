b068135cfeed2045a1bf18c49a8229bc35ceba08
David Santos <david.santos@fullstack.ec>
2017-01-12 09:07:49 -0500

    primeros componentes

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    var i = 2;
+    return(
+      <div>
+        <h1>Header</h1>
+        <h2>subheader</h2>
+        <p data-myatribute="sometext">
+          Hola mundo, osea jelou!!!
+        </p>
+        <p>{1+1}</p>
+        <p>{ i== 1 ? 'igual a 1':'2' }</p>
+      </div>
+    );
+  }
+}
+
+export default App;
