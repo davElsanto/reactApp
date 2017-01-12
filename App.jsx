fc9c65da1fba88152854da6116db88c991759bea
David Santos <david.santos@fullstack.ec>
2017-01-12 09:13:25 -0500

    cambios

--- App.jsx
+++ App.jsx
@@ -8,7 +8,7 @@ class App extends React.Component{
         <h1>Header</h1>
         <h2>subheader</h2>
         <p data-myatribute="sometext">
-          Hola mundo, osea jelou!!!
+          Hola mundo, osea jelou, deja el chou!!!
         </p>
         <p>{1+1}</p>
         <p>{ i== 1 ? 'igual a 1':'2' }</p>
