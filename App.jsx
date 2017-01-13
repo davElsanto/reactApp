9bdca7e5faa139898fe3c2af3173c4c93e92904a
David Santos <david.santos@fullstack.ec>
2017-01-13 16:43:20 -0500

    componentes anidados

--- App.jsx
+++ App.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 
 class App extends React.Component{
   render(){
-    var i = 2;
+    return(
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component{
+  render(){
     return(
       <div>
         <h1>Header</h1>
-        <h2>subheader</h2>
-        <p data-myatribute="sometext">
-          Hola mundo, osea jelou, deja el chou!!!
-        </p>
-        <p>{1+1}</p>
-        <p>{ i== 1 ? 'igual a 1':'2' }</p>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component{
+  render(){
+    return(
+      <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
       </div>
     );
   }
